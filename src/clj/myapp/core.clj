e6a7a60fe8d695215b205ce1dfc3e77f5ab52b7f
dimovich <dimovich@gmail.com>
2019-08-25 03:01:01 +0300

    use ctn.repl/refresh

--- src/clj/myapp/core.clj
+++ src/clj/myapp/core.clj
@@ -1,3 +1,1 @@
 (ns myapp.core)
-
-
--- watch.clj
+++ watch.clj
@@ -1,32 +1,31 @@
 (require '[figwheel.main.watching :as fww]
-         '[clojure.tools.namespace.reload :as nr]
-         '[clojure.tools.namespace.file :as nf]
-         '[clojure.tools.namespace.track :as nt])
+         '[clojure.tools.namespace.repl :as nr])
 
 
 
 (def clj-paths ["src/clj"])
 
-(println "watching clj: " (pr-str clj-paths))
 
-(fww/add-watch!
- :my-clj-watcher
- {:paths clj-paths
-  :filter (fww/suffix-filter #{"clj" "cljc"})
-  :handler (fww/throttle
-            100
-            (fn [evts]
-              (when-let [files (->> evts (mapv :file)
-                                    set vec not-empty)]
-                (doseq [file files]
-                  (let [err (-> (nt/tracker)
-                                (nf/add-files [file])
-                                (nr/track-reload)
-                                ::nr/error)]
-                    (if err
-                      (println (-> err ex-message)
-                               "\n" (-> err ex-cause ex-message))
-                      (println "reloaded " (.getPath file))))))))})
+
+(defn watch! []
+  (println "watching clj: " (pr-str clj-paths))
+
+  (apply nr/set-refresh-dirs clj-paths)
+
+  (fww/add-watch!
+   :my-clj-watcher
+   {:paths clj-paths
+    :filter (fww/suffix-filter #{"clj" "cljc"})
+    :handler (fww/throttle
+              100
+              (bound-fn [_]
+                (let [result (nr/refresh)]
+                  (when (not= :ok result)
+                    (println (ex-message result) "\n"
+                             (ex-message (ex-cause result)))))))}))
+
+
+(watch!)
 
 
 
