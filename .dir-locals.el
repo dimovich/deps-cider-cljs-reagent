ac1233a446a10dfb8adfee12f5aaaf31f94d41f6
dimovich <dimovich@gmail.com>
2022-01-08 18:37:32 +0200

    update clj deps alias invocation

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ((nil
-  (cider-clojure-cli-global-options . "-A:cider")
+  (cider-clojure-cli-global-options . "-M:cider")
   (cider-default-cljs-repl . figwheel-main)
   (cider-figwheel-main-default-options . ":dev")))
